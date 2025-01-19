269b056f79377d5c465aa0cd0baa00cc03592774
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2025-01-19 20:24:49 +0100

    Add Gc smart pointer, try dependencies marking. Issue: trace() can't mark itself...

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use std::alloc::{alloc, Layout};
 use std::cell::{Cell, RefCell};
+use std::fmt;
 use std::mem::align_of;
+use std::ops::Deref;
 use std::ptr::{self, NonNull};
 
+/// The garbage-collected heap.
 pub struct Heap {
     start: NonNull<u8>,
+    /// Pointer to the next free address in the heap. This address is always `BlockHeader`-aligned.
     current: Cell<NonNull<u8>>,
     roots: RefCell<Vec<NonNull<BlockHeader>>>,
     size: usize,
@@ -12,13 +16,17 @@ pub struct Heap {
 
 const MARK_BIT_MASK: usize = 1 << 63;
 
-pub struct BlockHeader {
-    // Size in bytes of the allocated chunk. The most significant bit holds the mark and sweep
-    // flag.
+/// The header of a heap-allocated value.
+struct BlockHeader {
+    /// Size in bytes of the allocated chunk, including the padding at the end of this header and
+    /// before the content, and including the padding at the end of the content (so that the next
+    /// entry is `BlockHeader`-aligned). Doesn't include the size of the header itself.
+    ///
+    /// The most significant bit holds the mark and sweep flag.
     size: usize,
-    // The padding between the header and the beginning of the object.
+    /// The padding between the header and the beginning of the object.
     padding: usize,
-    // Pointer to the callback function that traces the object.
+    /// Pointer to the callback function that traces the object.
     tracer: fn(*const u8),
 }
 
@@ -35,11 +43,42 @@ impl BlockHeader {
         self.size & MARK_BIT_MASK != 0
     }
 
+    /// Returns the size of the allocated chunk in bytes, filtering out the mark bit.
+    pub fn size(&self) -> usize {
+        self.size & !MARK_BIT_MASK
+    }
+
     pub fn trace(&self) {
         let value = unsafe {
             (ptr::from_ref(self) as *const u8).add(size_of::<BlockHeader>() + self.padding)
         };
         (self.tracer)(value)
+    }
+}
+
+pub struct Gc<T> {
+    data: NonNull<T>,
+}
+
+impl<T> Clone for Gc<T> {
+    fn clone(&self) -> Self {
+        Self { data: self.data }
+    }
+}
+
+impl<T> Copy for Gc<T> {}
+
+impl<T> Deref for Gc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { self.data.as_ref() }
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Gc<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Gc({:?})", self.data)
     }
 }
 
@@ -64,7 +103,7 @@ impl Heap {
 
             if size >= (isize::MAX as usize) {
                 panic!(
-                    "requested a heap size greater than max authorized (isize::MAX={}",
+                    "requested a heap size greater than max authorized (isize::MAX={})",
                     isize::MAX
                 );
             }
@@ -83,12 +122,16 @@ impl Heap {
         self.sweep();
     }
 
-    pub fn alloc_root<T: Trace>(&self, value: T) -> &mut T {
-        self.alloc_impl(value, true)
+    pub fn alloc_root<T: Trace>(&self, value: T) -> Gc<T> {
+        Gc {
+            data: self.alloc_impl(value, true).into(),
+        }
     }
 
-    pub fn alloc<T: Trace>(&self, value: T) -> &mut T {
-        self.alloc_impl(value, false)
+    pub fn alloc<T: Trace>(&self, value: T) -> Gc<T> {
+        Gc {
+            data: self.alloc_impl(value, false).into(),
+        }
     }
 
     fn alloc_impl<T: Trace>(&self, value: T, root: bool) -> &mut T {
@@ -96,8 +139,8 @@ impl Heap {
         let current = self.current.get().as_ptr();
 
         // We're out of memory. Let's try to collect.
-        // We are overly conservative with alignement, and use an upper bound instead of computing
-        // the exact value. It doesn't matter much for a few bytes.
+        // We are overly conservative with alignement padding and use an upper bound instead of
+        // computing the exact value. It doesn't matter much for a few bytes.
         if (current as usize)
             + layout.size()
             + size_of::<BlockHeader>()
@@ -106,22 +149,21 @@ impl Heap {
             >= (self.start.as_ptr() as usize) + self.size
         {
             self.collect();
-            return self.alloc(value);
+            return self.alloc_impl(value, root);
         }
 
         unsafe {
             let prev_cur = current;
             // We need to keep one bit for the mark and seep. It's initialized to zero by default.
-            assert!(layout.size() < (1 << 63));
+            assert!(layout.size() & MARK_BIT_MASK == 0);
             // Reserve space for the block header.
-            // We use the following invariant of the heap: `current` is always
-            // `BlockHeader`-aligned
+            // We maintain the following invariant: `current` is always `BlockHeader`-aligned
             let header_ptr = current as *mut BlockHeader;
 
             if root {
                 self.roots
                     .borrow_mut()
-                    // Safety: header_ptr is coming from `current`, which is NonNull
+                    // Safety: `header_ptr` is coming from `current`, which is NonNull
                     .push(NonNull::new_unchecked(header_ptr));
             }
 
@@ -130,22 +172,23 @@ impl Heap {
             // Safety:
             // 1. size_of::<BlockHeader>() is 8 currently and will never exceed a few words, so it
             //    won't overflow isize
-            // 2. The check at the beginning of this function ensures that we are still in boundary
-            //    of the same initial heap allocation.
+            // 2. The check at the beginning of this function ensures that we are still in the
+            //    boundaries of the same initial heap allocation.
             let unaligned_slot = current.add(size_of::<BlockHeader>());
 
             // Safety:
             // 1. layout.align() * sizeof::<u8>() doesn't overflow isize: we check this explicitly
             // 2. current is derived from the initial allocation in Heap::new(), and the check at
             //    the beginning of this function precisely ensures that when offset by
-            //    `layout.size() + size_of::<BlockHeader>()`, this remains in the original range of
-            //    allocated memory.
+            //    `layout.size() + size_of::<BlockHeader>()` plus potential padding, this remains
+            //    in the original range of allocated memory.
             assert!(layout.align() * size_of::<u8>() < (isize::MAX as usize));
             let slot = align_up(unaligned_slot, layout.align());
 
             // Safety: see the justification for the call to `align_up` above.
             // We maintain the  invariant that the next current pointer is `BlockHeader`-aligned.
             let next_slot = align_up(slot.add(layout.size()), align_of::<BlockHeader>());
+            // Safety: `next_slot` is an offset from the `self.current`, which is non null
             self.current.set(NonNull::new_unchecked(next_slot));
 
             // Now that we have the total size of the allocated chunk including padding, we can
@@ -181,8 +224,8 @@ impl Heap {
         unsafe {
             while ptr < end {
                 let header = &*(ptr as *mut BlockHeader);
-                println!("Next object: {} bytes (header @ {ptr:p})", header.size);
-                ptr = ptr.wrapping_add(header.size + size_of::<BlockHeader>());
+                println!("Next object: {} bytes (header @ {ptr:p})", header.size());
+                ptr = ptr.wrapping_add(header.size() + size_of::<BlockHeader>());
             }
         }
     }
@@ -229,7 +272,7 @@ unsafe fn align_up(ptr: *mut u8, align: usize) -> *mut u8 {
     //    That is `!remainder + 1` which is `align - remainder` if `remainder != 0`, or `align`
     //    otherwise.
     // 2. Take the modulo `align` again to get exactly `align - remainder`
-    // 3. Offset he ptr by this value
+    // 3. Offset the ptr by this value
     let offset = ((!(ptr as usize) & (align - 1)) + 1) & (align - 1);
     ptr.add(offset)
 }
@@ -238,7 +281,6 @@ unsafe fn align_up(ptr: *mut u8, align: usize) -> *mut u8 {
 /// This methods keeps the address provenance information.
 ///
 /// Requires that `align` is a power of 2.
-
 unsafe fn align_down(ptr: *mut u8, align: usize) -> *mut u8 {
     let offset = (ptr as usize) & (align - 1);
     ptr.sub(offset)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod lib;
 
-use bump_alloc::{Heap, Trace};
+use bump_alloc::{Gc, Heap, Trace};
 
 #[derive(Debug)]
 struct Baz {
@@ -9,9 +9,23 @@ struct Baz {
     thd: [u32; 4],
 }
 
+#[derive(Debug)]
+struct Foo {
+    fst: usize,
+    snd: String,
+    thd: Gc<Baz>,
+}
+
 impl Trace for Baz {
     fn trace(&self) {
         println!("Tracing baz");
+    }
+}
+
+impl Trace for Foo {
+    fn trace(&self) {
+        println!("Tracing foo");
+        self.thd.trace();
     }
 }
 
@@ -20,13 +34,27 @@ fn main() {
 
     let foo = heap.alloc(5);
     let bar = heap.alloc_root("hello, world".to_owned());
-    let baz = heap.alloc_root(Baz {
+    let baz = heap.alloc(Baz {
         fst: 0,
         snd: true,
         thd: [1, 2, 3, 4],
     });
 
-    println!("Alloced {foo}, {bar} and {baz:?}");
+    let baz_deps = heap.alloc(Baz {
+        fst: 1,
+        snd: false,
+        thd: [2, 1, 2, 1],
+    });
+
+    let foo_struct = heap.alloc_root(Foo {
+        fst: 42,
+        snd: "hello, world".to_owned(),
+        thd: baz_deps,
+    });
+
+    println!("Some operation {}", baz.fst + *foo + (baz.thd[0] as usize));
+
+    println!("Alloced {foo:?}, {bar:?}, {baz:?}, {baz_deps:?} and {foo_struct:?}");
 
     heap.collect();
     heap.collect();
