b5145b45748c69ecfcb2384ea58d8d351d795fa8
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2025-04-08 23:06:54 +0200

    Implement evacuation, but missing alignement handling

--- src/lib.rs
+++ src/lib.rs
@@ -11,21 +11,52 @@ pub struct Heap {
     /// The space for allocating new objects.
     young_space: HeapSpace,
     /// The space for evacuating objets that survived a young collection.
-    old_space: HeapSpace,
+    mature_space: HeapSpace,
     /// The current roots that the garbage collector traces.
     roots: RefCell<Vec<NonNull<BlockHeader>>>,
 }
 
-/// A heap space, a bump-allocated contiguous area.
+/// A heap from- or to-space, that is a bump-allocated contiguous area.
 pub struct HeapSpace {
     /// Pointer to the start of the young generation. This address is always `BlockHeader`-aligned.
     start: NonNull<u8>,
     /// Pointer to the next free address in the young generation. This address is always `BlockHeader`-aligned.
     current: Cell<NonNull<u8>>,
+    /// The size of the heap space in bytes.
     size: usize,
 }
 
-const MARK_BIT_MASK: usize = 1 << 63;
+/// Mask of the bit used in [BlockHeader::size] either by the mark-and-sweep garbage collector of
+/// the mature space to record marked objects or by the moving collector of the young space to
+/// record evacuated objects. Since we need to distinguish a forwarding pointer from the size of a
+/// non-forwarded object, it must be never be set in native pointers.
+///
+/// In order to use the same block header implementation uniformly, and since we need the forwarded
+/// bit to be set to `1` by default to mean non-forwarded (distinguishing it from pointers), we use
+/// `1` to mean NON marked and `0` to mean marked, perhaps counter-intuitively. Doing so, we can
+/// always initialize the mark bit to `1`.
+const MARK_BIT_MASK: usize = 1;
+
+/// A item of the collector work list.
+pub struct TraceEntry {
+    /// A pointer to the field of the object being traced, which is itself a pointer to a
+    /// garbage-collected object. Can be null for root objects or during mark-and-sweep collection.
+    field: *mut GcPtr,
+    /// The pointee of the object's field.
+    pointee: GcPtr,
+}
+
+impl TraceEntry {
+    pub fn pointee_only(pointee: GcPtr) -> Self {
+        TraceEntry {
+            field: ptr::null_mut(),
+            pointee,
+        }
+    }
+}
+
+/// A function pointer to an object implementing [Trace].
+pub type Tracer = fn(*const u8, &mut Vec<TraceEntry>);
 
 /// The header of a heap-allocated value.
 struct BlockHeader {
@@ -45,46 +76,98 @@ struct BlockHeader {
     ///
     /// `size` is `e - b` when interpreted as memory addresses.
     ///
-    /// The most significant bit holds the mark and sweep flag.
+    /// The least significant bit holds the mark and sweep or the forwarded flag, depending on the
+    /// space. So size is in the `usize-1` most significant bits of this field.
     size: usize,
     /// The padding between the header and the beginning of the object ((b) in the diagram
     /// describing [Self::size]).
     padding: usize,
     /// Pointer to the callback function that traces the object.
-    tracer: fn(*const u8, &mut Vec<GcPtr>),
+    tracer: Tracer,
 }
 
 impl BlockHeader {
-    /// Marks the block as reachable.
-    pub fn mark(&mut self) {
-        self.size = self.size | MARK_BIT_MASK;
+    pub fn new(size: usize, padding: usize, tracer: Tracer) -> Self {
+        Self {
+            size: (size << 1) | MARK_BIT_MASK,
+            padding,
+            tracer,
+        }
     }
 
-    pub fn unmark(&mut self) {
+    /// Marks the block as reachable.
+    pub fn mark(&mut self) {
         self.size = self.size & !MARK_BIT_MASK;
     }
 
+    /// Unmark the block after a collection.
+    pub fn unmark(&mut self) {
+        self.size = self.size | MARK_BIT_MASK;
+    }
+
+    /// Check if this block is marked.
     pub fn is_marked(&self) -> bool {
-        self.size & MARK_BIT_MASK != 0
+        (self.size & MARK_BIT_MASK) == 0
     }
 
-    /// Returns the size of the allocated chunk in bytes, filtering out the mark bit.
+    /// During a moving collection, overwrite the first word of the header (`size`) with a the new
+    /// address of this object in the mature space. Since pointers are at least 2-byte aligned, the
+    /// forwarding bit is zero, which distinguishes this block from an as-of-yet not moved block.
+    pub fn forward(&mut self, new_ptr: GcPtr) {
+        self.size = new_ptr.start.as_ptr() as usize
+    }
+
+    /// Checks if this block has been moved already during a moving collection.
+    pub fn is_forwarded(&self) -> bool {
+        self.is_marked()
+    }
+
+    /// Returns the forwarding address of the block if this block has already been moved during a
+    /// moving collection, or `None` otherwise.
+    pub fn forwarding_addr(&self) -> Option<GcPtr> {
+        self.is_forwarded().then_some(GcPtr {
+            start: NonNull::new(self.size as *mut BlockHeader).unwrap(),
+        })
+    }
+
+    /// Returns the size of the allocated chunk in bytes (accounting for everything but the header
+    /// size, see [Self::size] and [Self::full_size]), filtering out the mark bit.
     pub fn size(&self) -> usize {
-        self.size & !MARK_BIT_MASK
+        self.size >> 1
     }
 
+    /// Returns the total size of the allocation, from the beginning of the header to the end of
+    /// the value, including padding. This means that adding [Self::full_size] to `&self` points to
+    /// the next block header in the heap or to the next uninitialized memory slot if this block is
+    /// currently the last allocated.
+    pub fn alloc_total_size(&self) -> usize {
+        self.size() + std::mem::size_of::<BlockHeader>()
+    }
+
+    /// Trace and mark all reachable objects from this block.
     pub fn trace(&self) {
-        let mut stack = vec![GcPtr {
+        let mut stack = vec![TraceEntry::pointee_only(GcPtr {
             start: NonNull::new(ptr::from_ref(self) as *mut BlockHeader).unwrap(),
-        }];
+        })];
 
-        while let Some(gc) = stack.pop() {
+        while let Some(TraceEntry {
+            field: _,
+            pointee: gc,
+        }) = stack.pop()
+        {
             eprintln!("Trace loop: popping");
 
-            let value = unsafe {
-                (gc.start.as_ptr() as *const u8).add(size_of::<BlockHeader>() + self.padding)
-            };
+            // Safety: any pointer stored in the stack must be a block header pointer in one of the
+            // GC managed space. Such pointers are guaranteed to
+            //
+            // 1. Point to a valid block header
+            // 2. The block header is followed by a valid object at the end of the header + padding
+            //
+            // Thus the `add` operation sill end up within the bounds of the heap space.
             let header = unsafe { &mut *(gc.start.as_ptr()) };
+            let value = unsafe {
+                (gc.start.as_ptr() as *const u8).add(size_of::<BlockHeader>() + header.padding)
+            };
 
             if header.is_marked() {
                 continue;
@@ -95,28 +178,40 @@ impl BlockHeader {
         }
     }
 
-    pub fn evacuate(&self) {
-        todo!();
+    /// Evacuate this block to the mature space and return the address of the new copy.
+    pub fn evacuate(&mut self, to_space: &HeapSpace) -> GcPtr {
+        let self_ptr: *mut BlockHeader = self;
+        let new_addr = unsafe {
+            to_space.copy(GcPtr {
+                start: NonNull::new_unchecked(self_ptr),
+            })
+        };
 
-        let mut stack = vec![GcPtr {
-            start: NonNull::new(ptr::from_ref(self) as *mut BlockHeader).unwrap(),
-        }];
+        let mut stack = vec![TraceEntry::pointee_only(GcPtr {
+            start: NonNull::new(new_addr.start.as_ptr()).unwrap(),
+        })];
 
-        while let Some(gc) = stack.pop() {
-            eprintln!("Trace loop: popping");
+        while let Some(TraceEntry { pointee, field }) = stack.pop() {
+            eprintln!("Evacuate loop: popping");
 
-            let value = unsafe {
-                (gc.start.as_ptr() as *const u8).add(size_of::<BlockHeader>() + self.padding)
-            };
-            let header = unsafe { &mut *(gc.start.as_ptr()) };
+            let from_header = unsafe { &mut *(pointee.start.as_ptr()) };
 
-            if header.is_marked() {
-                continue;
+            let new_addr = from_header.forwarding_addr().unwrap_or_else(|| unsafe {
+                let to_addr = to_space.copy(pointee);
+                let to_header = &*(to_addr.start.as_ptr());
+                from_header.forward(to_addr);
+                let to_value = (to_addr.start.as_ptr() as *const u8)
+                    .add(size_of::<BlockHeader>() + to_header.padding);
+                (to_header.tracer)(to_value, &mut stack);
+                to_addr
+            });
+
+            unsafe {
+                field.write(new_addr);
             }
-
-            header.mark();
-            (header.tracer)(value, &mut stack);
         }
+
+        new_addr
     }
 }
 
@@ -173,7 +268,7 @@ impl<T: fmt::Debug> fmt::Debug for Gc<T> {
 }
 
 pub trait Trace {
-    fn trace(&self, _stack: &mut Vec<GcPtr>) {}
+    fn trace(&self, _stack: &mut Vec<TraceEntry>) {}
 }
 
 impl Trace for usize {}
@@ -184,7 +279,7 @@ impl Heap {
     pub fn new(young_size: usize, old_size: usize) -> Self {
         Self {
             young_space: HeapSpace::new(young_size),
-            old_space: HeapSpace::new(old_size),
+            mature_space: HeapSpace::new(old_size),
             roots: RefCell::new(Vec::new()),
         }
     }
@@ -239,8 +334,8 @@ impl Heap {
     //                   println!("Object {ptr:p} is unmarked. Sweeping (in principle, currently unimplemented)");
     //               }
     //
-    //               println!("Next object: {} bytes (header @ {ptr:p})", header.size);
-    //               ptr = ptr.wrapping_add(header.size + size_of::<BlockHeader>());
+    //               println!("Next object: {} bytes (header @ {ptr:p})", header.size());
+    //               ptr = ptr.wrapping_add(header.size() + size_of::<BlockHeader>());
     //           }
     //       }
     //   }
@@ -294,6 +389,10 @@ impl HeapSpace {
         }
     }
 
+    pub fn end(&self) -> *mut u8 {
+        self.start.as_ptr().wrapping_add(self.size)
+    }
+
     /// Checks if there is enough space to allocate a value of type `T` in this space.
     pub fn can_alloc<T: Trace>(&self) -> bool {
         let layout = Layout::new::<T>();
@@ -306,7 +405,15 @@ impl HeapSpace {
             .wrapping_add(size_of::<BlockHeader>())
             .wrapping_add(align_of::<BlockHeader>() - 1)
             .wrapping_add(layout.align() - 1)
-            < self.start.as_ptr().wrapping_add(self.size)
+            < self.end()
+    }
+
+    /// Checks if there is enough space to allocate a new object as a copy of an existing one.
+    pub fn can_copy(&self, from: GcPtr) -> bool {
+        let current = self.current.get().as_ptr();
+        let header = unsafe { from.start.as_ref() };
+
+        current.wrapping_add(header.alloc_total_size()) < self.end()
     }
 
     /// Allocates an object in this space, or returns `None` if the space is full.
@@ -376,6 +483,29 @@ impl HeapSpace {
             Gc {
                 start: NonNull::new_unchecked(header_ptr),
                 _marker: std::marker::PhantomData,
+            }
+        }
+    }
+
+    /// Copy an existing [GcPtr], potentially from a different space, to this space.
+    //TODO: this is wrong! We can't guarantee that blindly copying the header and the value will
+    //preserve the value's alignement. Take a value with an alignment `a` bigger than
+    //`BlockHeader`'s alignement. By chance the end of the original header could end exactly at an
+    //`a`-aligned address, but nothing guarantees that this is the case in the two-space.
+    pub fn copy(&self, from: GcPtr) -> GcPtr {
+        if !self.can_copy(from) {
+            panic!("out of memory");
+        }
+
+        let current = self.current.get().as_ptr();
+        let full_size = unsafe { from.start.as_ref().alloc_total_size() };
+
+        unsafe {
+            std::ptr::copy(from.start.as_ptr() as *const u8, current, full_size);
+            let next_slot = current.add(full_size);
+            self.current.set(NonNull::new_unchecked(next_slot));
+            GcPtr {
+                start: NonNull::new_unchecked(current as *mut BlockHeader),
             }
         }
     }
